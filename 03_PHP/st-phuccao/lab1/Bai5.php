2dc06434265872ebaa784d87c988916ca05e26f1
phucchp <phuc.cao@supremetech.vn>
2023-09-11 11:54:19 +0700

    fix bai 5

--- 03_PHP/st-phuccao/lab1/Bai5.php
+++ 03_PHP/st-phuccao/lab1/Bai5.php
@@ -4,7 +4,7 @@ function doubleChar($str)
 {
     $rs = "";
     for ($i = 0; $i < strlen($str); $i++) {
-        $rs = $rs . $rs[$i] . $rs[$i];
+        $rs = $rs . $str[$i] . $str[$i];
     }
     echo $rs;
 }
