60ecddff891fcfa4df3faabb2f777a463b30e2a0
st-baodang <bao.dang@supremetech.vn>
2023-09-12 17:27:24 +0700

    Fix: Self-review bug

--- 03_PHP/st-baodang/lab-2/exercise1/Utils.php
+++ 03_PHP/st-baodang/lab-2/exercise1/Utils.php
@@ -8,14 +8,13 @@ function isWeekend(DateTime $date)
 // Kiểm tra xem hôm nay có lịch hẹn không
 function checkMeeting()
 {
-    if (date('d') != 20) {
+    if (date('d') != 12) {
         $day = date('d') < 20 ? 20 - date('d') : 20 + date('t') - date('d');
         echo "Cách lần gặp kế tiếp còn $day ngày";
     } elseif (date('D') === "Sun") {
         echo "Có lịch hẹn vào ngày hôm nay";
     } else {
-        $nextSunday = date('d', strtotime("next sunday"));
-        $day = $nextSunday - date('d');
-        echo "Lịch hẹn sẽ dời đến ngày $nextSunday, cách hiện tại còn $day ngày";
+        $nextSunday = date('d-M-Y', strtotime("next sunday"));
+        echo "Lịch hẹn sẽ dời đến ngày $nextSunday";
     }
 }
