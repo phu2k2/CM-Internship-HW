a47d70cf7e8e0d57f6cadf5c0ced686377de6c37
cmvn-phunguyen2 <phu.nguyen@classmethod.vn>
2023-09-13 23:02:08 +0700

    Finish PHP Advanced

---
+++ 03_PHP/cm-phunguyen/lab_2/Ex4.php
@@ -0,0 +1,24 @@
+<?php
+function checkFormat($string)
+{
+    if (empty($string)) {
+        throw new Exception("Biến empty", 400);
+    }
+    if (!is_string($string)) {
+        throw new Exception("Biến không phải kiểu String", 500);
+    }
+    if (filter_var($string, FILTER_VALIDATE_EMAIL)) {
+        return 'email';
+    }
+    if (filter_var($string, FILTER_VALIDATE_URL)) {
+        return 'url';
+    }
+}
+try {
+    $string = 2;
+    $result = checkFormat($string);
+    echo $result;
+} catch (Exception $e) {
+    echo 'Message: ', $e->getMessage();
+    echo ' - Code: ', $e->getCode();
+}
