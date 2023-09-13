f52a819622046f41099c2ccbc4dcb46c864b4793
st-huyennguyen <huyen.nguyen@supremetech.vn>
2023-09-13 11:01:48 +0700

    fix exercise_2.1.php

--- 03_PHP/st-huyennguyen/Lab-2/Exercise-2/exercise_2.1.php
+++ 03_PHP/st-huyennguyen/Lab-2/Exercise-2/exercise_2.1.php
@@ -1,12 +1,12 @@
 <?php
-function creteFile()
+function creteFile($name,$description)
 {
     $myProfile = fopen(__DIR__ . "/myprofile.txt", "w");
-    $name = "Huyen Nguyen T. C.\n";
-    $description = "I'm 22 years old. I like reading, drawing and crocheting.\n";
     fwrite($myProfile, $name);
     fwrite($myProfile, $description);
     fclose($myProfile);
 }
 
-creteFile();
+$name = "My fullname is Nguyen Thi Cam Huyen\n";
+$description = "I'm 22 years old. I like reading, drawing and crocheting.\n";
+creteFile($name, $description);
