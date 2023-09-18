1586faa4da06616dc4924a076f2e16dd6ed09247
thinhtran <thinh.tran@classmethod.vn>
2023-09-18 15:05:01 +0700

    lab_3 php oop

---
+++ 03_PHP/cm-thinhtran/lab_3/bai4.php
@@ -0,0 +1,12 @@
+<?php
+    class ParentClass {
+        final public function speak() {
+            echo "Hello";
+        }
+    }
+    
+    final class ChildClass extends ParentClass {
+        // public function speak() {}
+    }
+    // class GrandChildClass extends ChildClass {}
+?>
