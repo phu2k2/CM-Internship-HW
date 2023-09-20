f40015e30889b2bdcd9fb58625cc7c4cc4fac6ab
thanhvu <vu.tran@classmethod.vn>
2023-09-20 08:39:02 +0700

    Submit lab create migration

---
+++ 04_Laravel/cm-vutran/database/migrations/2023_09_19_085540_create_order_details_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orderdetails', function (Blueprint $table) {
+            $table->unsignedBigInteger('id');
+            $table->unsignedBigInteger('invoice_id');
+            $table->char('product_id', 4);
+            $table->decimal('price', 10, 2);
+            $table->integer('amount');
+            $table->decimal('discount', 10, 2);
+
+            $table->primary(['id', 'product_id']);
+
+            $table->foreign('invoice_id')
+                ->references('id')
+                ->on('orders')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('product_id')
+                ->references('product_id')
+                ->on('products')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');    
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
