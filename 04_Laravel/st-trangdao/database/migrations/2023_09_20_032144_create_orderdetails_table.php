6997e4446f293fe4c09fcc46835c428821ebb391
st-trangdao <trang.dao@supremetech.vn>
2023-09-22 09:25:28 +0700

    fix migrations

--- 04_Laravel/st-trangdao/database/migrations/2023_09_20_032144_create_orderdetails_table.php
+++ 04_Laravel/st-trangdao/database/migrations/2023_09_20_032144_create_orderdetails_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('order_details', function (Blueprint $table) {
+        Schema::create('orderdetails', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('invoice_id');
             $table->char('product_id', 4);
@@ -37,6 +37,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('order_details');
+        Schema::dropIfExists('orderdetails');
     }
 };
