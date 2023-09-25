86dfddf09d7d5f831a4437390a45a93e288d4a93
st-quangtran2 <quang.tran2@supremetech.vn>
2023-09-25 11:41:49 +0700

    add route web

--- 04_Laravel/st-quangtran/routes/web.php
+++ 04_Laravel/st-quangtran/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\SupplierController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +18,45 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.pages.dashboard');
 });
+
+Route::get('dashboard', function () {
+    return view("admin.customer.show");
+});
+
+//customer
+Route::get("customer", [CustomerController::class, "index"]);
+
+Route::get("/customer/insert", [CustomerController::class, "create"]);
+
+Route::post('/store', [CustomerController::class, 'store']);
+
+Route::get("/customer/update/{id}", [CustomerController::class, "edit"]);
+
+//category
+Route::get("category", [CategoryController::class, "index"]);
+
+Route::get("category/insert", [CategoryController::class, "create"]);
+
+Route::post('/store', [CategoryController::class, 'store']);
+
+Route::get("/category/update/{id}", [CategoryController::class, "edit"]);
+
+//employee
+Route::get("employee", [EmployeeController::class, "index"]);
+
+Route::get("employee/insert", [EmployeeController::class, "create"]);
+
+Route::post('/store', [EmployeeController::class, 'store']);
+
+Route::get("employee/update/{id}", [EmployeeController::class, "edit"]);
+
+//supplier
+Route::get("supplier", [SupplierController::class, "index"]);
+
+Route::get("supplier/insert", [SupplierController::class, "create"]);
+
+Route::post('/store', [SupplierController::class, 'store']);
+
+Route::get("supplier/update/{id}", [SupplierController::class, "edit"]);
