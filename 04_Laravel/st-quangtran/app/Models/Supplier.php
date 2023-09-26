1345d10c3c7411bab0244b4ae614d2323e1f7447
st-quangtran2 <quang.tran2@supremetech.vn>
2023-09-26 16:32:19 +0700

    model and seed

---
+++ 04_Laravel/st-quangtran/app/Models/Supplier.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    use HasFactory;
+    protected $table = "suppliers";
+
+    protected $fillable = [
+        'company_id',
+        'company_name',
+        'transaction_name',
+        'address',
+        'phone',
+        'fax',
+        'email',
+    ];
+}
