d2f716c766a8d2e83dd8801001ccc0c38640951f
st-trangdao <trang.dao@supremetech.vn>
2023-09-28 16:44:21 +0700

    submit

--- 04_Laravel/st-trangdao/resources/views/suppliers/index.blade.php
+++ 04_Laravel/st-trangdao/resources/views/suppliers/index.blade.php
@@ -44,7 +44,6 @@
                                             href="{{ route('suppliers.edit', ['supplier' => $supplier['id']]) }}">
                                             <i class="bx bx-edit-alt me-1"></i> Edit</a>
                                         <a class="dropdown-item">
-                                            <i class="bx bx-trash me-1"></i> Delete</a>
                                             <a class="dropdown-item" data-bs-toggle="modal"
                                             data-bs-target="#modalCenter{{ $supplier['id'] }}">
                                             <i class="bx bx-trash me-1"></i> Delete</a>
