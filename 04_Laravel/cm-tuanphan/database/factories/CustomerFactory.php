4371ff8bd694c0b28f8d9bfca38bcfe9ea6892c8
cmvn-tuanphan <tuan.phan@classmethod.vn>
2023-10-02 17:05:55 +0700

    fix universal faker call and make slightly modfication

--- 04_Laravel/cm-tuanphan/database/factories/CustomerFactory.php
+++ 04_Laravel/cm-tuanphan/database/factories/CustomerFactory.php
@@ -17,12 +17,12 @@ class CustomerFactory extends Factory
     public function definition(): array
     {
         return [
-            'company_name' => fake()->text(50),
+            'company_name' => fake('vi_VN')->company(),
             'transaction_name' => strtoupper(fake()->text(20)),
-            'address' => fake()->text(50),
-            'email' => fake()->email,
-            'phone_number' => $this->faker->numerify('##########'),
-            'fax' => $this->faker->numerify('##########'),
+            'address' => fake('vi_VN')->city() . "  " . fake('vi_VN')->province(),
+            'email' => fake()->unique()->companyEmail(),
+            'phone_number' => fake()->numerify('##########'),
+            'fax' => fake()->numerify('##########'),
         ];
     }
 }
