8243eb6090be4160474ac3e03b73a8eae63bd958
phucchp <phuc.cao@supremetech.vn>
2023-10-03 09:53:10 +0700

    done ex7

--- 04_Laravel/st-phuccao/app/Http/Controllers/HomeController.php
+++ 04_Laravel/st-phuccao/app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Customer;
+use App\Models\Employee;
+use App\Models\Order;
+use App\Models\OrderDetail;
 use App\Models\Product;
 use App\Models\Supplier;
 use Illuminate\Contracts\Database\Eloquent\Builder;
@@ -13,7 +16,7 @@ class HomeController extends Controller
 {
     public function practice()
     {
-        return $this->exciseThree();
+        return $this->exciseSix();
     }
 
     public function exciseOne()
@@ -26,7 +29,7 @@ class HomeController extends Controller
 
     public function exciseTwo()
     {
-        \DB::enableQueryLog();
+        // \DB::enableQueryLog();
 
         Supplier::whereHas('products.category', function ($query) {
             $query->where('category_name', 'LIKE', '%Thực phẩm%');
@@ -34,7 +37,7 @@ class HomeController extends Controller
         ->select('company_id', 'company_name', 'address')
         ->get();
 
-        dd(\DB::getQueryLog());
+        // dd(\DB::getQueryLog());
 
     }
 
@@ -47,9 +50,49 @@ class HomeController extends Controller
         // }
         $customersByProductName = Product::where('product_name','LIKE', '%Sữa hộp%')
             ->firstOrFail()
-            ->orderDetails();
+            ->orderDetails()->with('orders');
             // ->get();
         // // return $milk;
         return $customersByProductName;
+    }
+
+    public function exciseFour()
+    {
+        $order = Order::with(['customer','employee'])->find(1); 
+        return [
+            'Customer' => $order->customer,
+            'Employee' => $order->employee,
+            'Delivery Time' => $order->delivery_date,
+            'Delivery Location' => $order->destination,
+        ];
+    }
+
+    public function exciseFive()
+    {
+        return Employee::selectRaw(
+            'id,
+            first_name,
+            last_name,
+            (base_salary + allowance) AS total_salary'
+        )
+        ->get();
+    }
+
+    public function exciseSix()
+    {
+        $orderNumber = 3;
+
+        $orderDetails = OrderDetail::with('product')->find($orderNumber);
+        return $orderDetails;
+    }
+
+    public function exciseSeven()
+    {
+        $partners = Customer::select('company_name')
+        ->whereIn('transaction_name', function ($query) {
+            $query->select('transaction_name')
+                ->from('suppliers');
+        })
+        ->get();
     }
 }
