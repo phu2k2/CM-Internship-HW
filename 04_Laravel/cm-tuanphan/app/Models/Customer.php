fd769221a2236bf61260f30dd3d336b1c3f1226d
cmvn-tuanphan <tuan.phan@classmethod.vn>
2023-10-04 11:49:22 +0700

    remove unnessessary code

--- 04_Laravel/cm-tuanphan/app/Models/Customer.php
+++ 04_Laravel/cm-tuanphan/app/Models/Customer.php
@@ -9,9 +9,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Customer extends Model
 {
     use HasFactory, SoftDeletes;
-    // SoftDeletes are implemented to prevent directly deleting Customers from the database
-    // To enable the use of SoftDeletes, please run the migration file:
-    // 2023_09_28_023151_add_soft_deletes_for_existing_tables.php
 
     protected $fillable = [
         'company_name',
--- 04_Laravel/cm-tuanphan/app/Models/Employee.php
+++ 04_Laravel/cm-tuanphan/app/Models/Employee.php
@@ -9,12 +9,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Employee extends Model
 {
     use HasFactory, SoftDeletes;
-    // SoftDeletes are implemented to prevent directly deleting Employees from the database
-    // To enable the use of SoftDeletes, please run the migration file:
-    // 2023_09_28_023151_add_soft_deletes_for_existing_tables.php
-
     protected $primaryKey = "employee_id";
+    
+    // The 'keyType' set to "string" allows the edit route to derive the primary key from the model
     protected $keyType = "string";
+
     protected $fillable = [
         'employee_id',
         'last_name',
